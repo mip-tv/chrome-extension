f12428d3ca4f678b45bddb42aa5976aa871463a3
eightsq <ok@electronvo.lt>
2017-11-25 17:07:56 +0100

    Added code comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (activeTab) {
-    var newURL = "https://jugendhackt.de";
+    var newURL = "https://jugendhackt.de"; // TODO redirect to our web app
     chrome.tabs.create({
         url: newURL
     });
@@ -7,12 +7,16 @@ chrome.browserAction.onClicked.addListener(function (activeTab) {
 });
 
 // Tracking code
+
+// defined constants
 var API_SYNC_URL = "http://188.68.58.69:8080/upload";
 var LOG_SERVER = "188.68.58.69";
 
+// create buffers as global variables
 var requestBuffer = [];
 var requestPreparedBuffer = [];
-function syncBuffer() {
+
+function syncBuffer() { // moves primary buffer to secondary buffer and clears; sends data to server
     requestPreparedBuffer = requestBuffer;
     requestBuffer = [];
     var postData = {
@@ -23,20 +27,22 @@ function syncBuffer() {
     syncRequest.open("POST", API_SYNC_URL, true);
     syncRequest.send(JSON.stringify(postData));
 }
-function logRequest(reqDetails) {
-    if (reqDetails.ip !== LOG_SERVER && reqDetails.ip !== undefined) {
-        var newEntry = [
+function logRequest(reqDetails) { // writes data of request (reqDetais) formatted to buffer
+    if (reqDetails.ip !== LOG_SERVER && reqDetails.ip !== undefined) { // make sure it is not our request (or local request)
+        var newEntry = [ // form as array
             reqDetails.ip,
             reqDetails.url,
             reqDetails.type,
             reqDetails.timeStamp
         ];
-        requestBuffer.push(newEntry);
-        if (requestBuffer.length >= 1000) {
+        requestBuffer.push(newEntry); // write to buffer
+        if (requestBuffer.length >= 1000) { // buffer over 1000 entries -> sync to server
             syncBuffer();
         }
     }
 }
+
+// listen for all network requests (all urls), for each -> logRequest()
 chrome.webRequest.onResponseStarted.addListener((details)=>{ logRequest(details) }, { urls: ["<all_urls>"] }, []);
 
-setInterval(syncBuffer, 4*60*1000);
+setInterval(syncBuffer, 4*60*1000); // sync buffer every 4 minutes
